4b3f8f28b73e139c576ccefbb17b0e9e2c11e627
Vijay Mewada <vijaymewada2012@gmail.com>
2020-04-20 19:20:51 +0530

    Updates

---
+++ precache-manifest.c0295eb938d863223cf278297f05bd08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8782b07835b7d44bd2bb31379c6c05a",
+    "url": "/schoolws/index.html"
+  },
+  {
+    "revision": "ac28b39d1c6304525be3",
+    "url": "/schoolws/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5ba58696cb0edb7a9764",
+    "url": "/schoolws/static/css/main.8baf6250.chunk.css"
+  },
+  {
+    "revision": "ac28b39d1c6304525be3",
+    "url": "/schoolws/static/js/2.827dbab6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/schoolws/static/js/2.827dbab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba58696cb0edb7a9764",
+    "url": "/schoolws/static/js/main.3bc9f6fa.chunk.js"
+  },
+  {
+    "revision": "539613a32941b0516de4",
+    "url": "/schoolws/static/js/runtime-main.4851aee4.js"
+  }
+]);
